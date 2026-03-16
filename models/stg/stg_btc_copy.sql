60b703e04815f2e40808f61ad3030d56bda6ac52
Raunaq Salve <raunaqs@amdocs.com>
2026-03-16 15:08:14 +0530

    stg_btc_copy

---
+++ models/stg/stg_btc_copy.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='incremental',incremental_strategy="merge",unique_key='HASH_KEY')}}
+
+select
+* 
+from 
+{{ source('btc','btc')}}
+
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+where BLOCK_TIMESTAMP >= (select max(BLOCK_TIMESTAMP) from {{ this }} )
+
+{% endif %}
